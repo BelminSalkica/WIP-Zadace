ec51934efd513f13671a9d2d9a319268874cd29b
Belmin Salkica <belminsalkica@gmail.com>
2016-08-25 17:47:31 +0200

    Implementing custom cocoa control: VKChekcbox

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -14,10 +14,17 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
+    @IBOutlet weak var checkbox: VKCheckbox!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Simple checkbox callback
+        checkbox.checkboxValueChangedBlock = {
+            isOn in
+        }
+        
         // Handle user inputs
         nameTextField.delegate = self
     }
